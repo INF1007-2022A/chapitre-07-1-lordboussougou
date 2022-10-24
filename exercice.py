3fce3ebd1b8bd86f75cc8570993ebf4d7f07e3f4
lordboussougou <113210400+lordboussougou@users.noreply.github.com>
2022-10-24 13:13:41 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -7,8 +7,29 @@
 
 # TODO: Définissez vos fonction ici
 
+#4
+def quatre_a(chaine1):
+    set1 = {'a', 'c', 'g', 't'}
+    chaine2 = set(chaine1)
+    return chaine2.issubset(set1)
 
-if __name__ == '__main__':
-    # TODO: Appelez vos fonctions ici
+def quatre_b():
+    while True:
+        chaine = input("Veuillez saisr une chaine ADN : ")
+        if quatre_a(chaine):
+            break
 
-    pass
+def proportion(chaine, seq):
+    return (len(seq)*chaine.count(seq) /len(chaine) )*100
+
+
+
+# TODO: Appelez vos fonctions ici
+
+chaine = "attgcaatggtggtacatg"
+séquence = "ca"
+p = proportion(chaine,séquence)
+print(f"Il y a {p:2f} % de {séquence}.")
+
+
+
